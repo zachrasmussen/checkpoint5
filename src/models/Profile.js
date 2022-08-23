3489ac16b206cd542887b1af13c02ad1b7342a1f
Zach Rasmussen <zsrasmussen@gmail.com>
2022-08-23 09:06:45 -0600

    checkpoint5

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -6,9 +6,9 @@ export class Profile {
         this.picture = data.picture
         this.bio = data.bio || 'Hey there, thanks for checking out my page! I still have a few things to upload, hopefully the next time you visit I will have my bio!'
         this.coverImg = data.coverImg
-        this.github = data.github
-        this.linkedin = data.linkedin
-        this.resume = data.resume
+        this.github = data.github || 'https://github.com/'
+        this.linkedin = data.linkedin || 'https://www.linkedin.com/'
+        this.resume = data.resume || 'https://thiscatdoesnotexist.com/'
         this.classYear = data.class || ''
         // FIXME turn or statement into turnary statement to represent graduated
         this.graduated = data.graduated || ''
